caa3362b813cf8a848be51aa1179942cb0fc794e
Oscar <oscarmunozhidalgo@gmail.com>
2024-03-03 15:45:18 +0000

    User profile dynamically loads data from jsons and input file image also is finished

--- WizQuizz/js-files/login/user-profile.js
+++ WizQuizz/js-files/login/user-profile.js
@@ -1,17 +1,31 @@
-let user = "null";
-var data;
+const nickname_display = document.getElementById("username")
+const user_image_display = document.getElementById("user-image-button")
+const user_description_display = document.querySelector(".description p")
+const account_date_display = document.querySelector(".account-creation-date p")
+const quizs_finished_display = document.querySelector(".quizs-finished p")
+const input_image = document.getElementById("image-input-file")
 
 fetch("../../data/current_session.json").then(res => res.json()).then(data => {
-    console.log(data);
+    nickname_display.innerHTML = data.user.nickname;
+    user_image_display.src = data.user.profile_picture;
+    user_description_display.innerHTML = data.user.user_description;
+    account_date_display.innerHTML = "MEMBER SINCE " + data.user.account_creation;
+    quizs_finished_display.innerHTML = data.user.quizs_played + " QUIZS FINISHED";
 });
 
-alert();
-
-window.onload = function(){
-    document.getElementById("username").innerHTML = "PaquitoElChocolatero";
+user_image_display.onclick = function(){
+    input_image.click();
 }
 
-document.getElementById('user-image-button').onclick = function(){
-    document.getElementById('image-input-file').click();
+input_image.onchange = function (evt){
+    var tgt = evt.target || window,
+        files = tgt.files;
+    
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            user_image_display.src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    }
 }
-
