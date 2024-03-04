eb4ca3cc3ac8439a317c6c3fe3d411ef0d319b30
Itamar <itamarreyrguez@gmail.com>
2024-03-04 16:59:48 +0000

    quizz-finish dynamic done and json included

--- WizQuizz/js-files/play/quizz-finish.js
+++ WizQuizz/js-files/play/quizz-finish.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', async function() {
     const finish = document.getElementById("finish");
+    const mark = document.getElementById("mark");
     var resultado = localStorage.getItem("results");
     var numofquestions = localStorage.getItem("numofquestions");
     var varInteger = parseInt(numofquestions);
-    var intResult = varInteger-resultado;
+    const jsonFilePath = "../../data/play/quizz-finish.json";
 
-    if(resultado >= varInteger/2){
-        mark.innerHTML = resultado + "/" + numofquestions;
-    }else{
-        mark.innerHTML = resultado  + "/" + numofquestions;
-    }
-
-    if(resultado === "1"){
-        finish.innerHTML = "Congratulations!";
-    }else{
-        finish.innerHTML = "Bad Luck!";
-    }
+    // Hacer la solicitud HTTP usando fetch
+    fetch(jsonFilePath)
+        .then(response => response.json())
+        .then(data => {
+            const resultWell = data.finishQuizz["result-well"];
+            const resultBad = data.finishQuizz["result-bad"];
+            
+            if(resultado >= varInteger/2){
+                mark.innerHTML = resultado + "/" + numofquestions;
+            }else{
+                mark.innerHTML = resultado  + "/" + numofquestions;
+            }
+        
+            if(resultado === "1"){
+                finish.innerHTML = resultWell;
+            }else{
+                finish.innerHTML = resultBad;
+            }
+        })
+        .catch(error => {
+            console.error('Error al cargar el archivo JSON:', error);
+    }); 
 });
