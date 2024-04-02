2d2bf3916cf6a41ff7611993062341f9119bfdee
Mauro Gómez <m.gomeznitoni@gmail.com>
2024-04-02 11:03:53 +0100

    Angular advances

--- WizQuizz-Angular/src/app/app.module.ts
+++ WizQuizz-Angular/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
