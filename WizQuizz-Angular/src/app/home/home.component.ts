d13bba74cc9c538ca5d9e5bbbf7689d2ca137159
Mauro Gómez <m.gomeznitoni@gmail.com>
2024-04-03 10:13:22 +0100

    Fixed loading issue in both pages

--- WizQuizz-Angular/src/app/home/home.component.ts
+++ WizQuizz-Angular/src/app/home/home.component.ts
@@ -6,82 +6,80 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrl: './home.component.css',
   encapsulation: ViewEncapsulation.None
 })
-
 export class HomeComponent implements OnInit {
-
   loader: HTMLElement | null = null;
 
-  ngOnInit() {
-    window.addEventListener("load", () => {
-      this.loader = document.querySelector(".loader");
-      if (this.loader) {
-        this.loader.classList.add("loader-hidden");
-        this.loader.addEventListener("transitionend", () => {
-          if (this.loader) {
-            document.body.removeChild(this.loader);
-          }
-        });
-      }
-    });
+  async ngOnInit() {
+    this.loader = document.querySelector(".loader");
+    if (this.loader) {
+      this.loader.classList.add("loader-hidden");
+      this.loader.addEventListener("transitionend", () => {
+        if (this.loader) {
+          document.body.removeChild(this.loader);
+        }
+      });
+    }
 
-    document.addEventListener('DOMContentLoaded', async () => {
-
+    try {
       const [whoData, newsData] = await Promise.all([
-        loadJSON({file: 'assets/jsons/home/who_content.json'}),
-        loadJSON({file: 'assets/jsons/home/news_content.json'})
+        this.loadJSON('assets/jsons/home/who_content.json'),
+        this.loadJSON('assets/jsons/home/news_content.json')
       ]);
 
-      renderContent({content: whoData.info, containerSelector: '.about-us-content'});
-      renderContent({content: newsData.news, containerSelector: '.news-content'});
+      this.renderContent(whoData.info, '.about-us-content');
+      this.renderContent(newsData.news, '.news-content');
 
       const hiddenElements = document.querySelectorAll('.hidden');
-      hiddenElements.forEach((el) => observer.observe(el));
-    });
+      hiddenElements.forEach((el) => this.observer.observe(el));
+    } catch (error) {
+      console.error('Error loading JSON data:', error);
+    }
+  }
 
-    async function loadJSON({file}: { file: any }) {
-      const response = await fetch(file);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch JSON: ${response.statusText}`);
+  async loadJSON(file: string) {
+    console.log("Reading JSON");
+    const response = await fetch(file);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON: ${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  renderContent(content: any[], containerSelector: string) {
+    const container = document.querySelector(containerSelector);
+    if (!container) return;
+
+    let count_news = 0;
+    content.forEach((item: any) => {
+      const div = document.createElement('div');
+      if (containerSelector === '.about-us-content') {
+        div.classList.add('about-us-info', 'hidden');
+        div.innerHTML = count_news % 2 === 0 ?
+          `<img src="${item.image}" width="320" height="180" class="image" alt="">
+           <p>${item.text}</p>` :
+          `<p>${item.text}</p>
+           <img src="${item.image}" width="320" height="180" class="image" alt="">`;
+        count_news++;
+      } else if (containerSelector === '.news-content') {
+        div.classList.add('new');
+        div.innerHTML = `<img src="${item.image}" width="560" height="315" alt="">
+                         <div class="news-description">
+                           <a href=""><h2>${item.headline}</h2></a>
+                           <p>${item.description}</p>
+                         </div>`;
       }
-      return response.json();
-    }
-
-    function renderContent({content, containerSelector}: { content: any, containerSelector: any }) {
-      const container = document.querySelector(containerSelector);
-      let count_news = 0;
-      content.forEach((item: any) => {
-        const div = document.createElement('div');
-        if (containerSelector === '.about-us-content') {
-          div.classList.add('about-us-info');
-          div.classList.add('hidden');
-          if (count_news % 2 === 0) {
-            div.innerHTML = `<img src="${item.image}" width="320" height="180" class="image" alt="">
-                                <p>${item.text}</p>`;
-            count_news++;
-          } else {
-            div.innerHTML = `<p>${item.text}</p>
-                                <img src="${item.image}" width="320" height="180" class="image" alt="">`;
-            count_news++;
-          }
-        } else if (containerSelector === '.news-content') {
-          div.classList.add('new');
-          div.innerHTML = `<img src="${item.image}" width="560" height="315" alt=""></a>
-                             <div class="news-description"><a href=""><h2>${item.headline}</h2></a>
-                             <p>${item.description}</p></div>`;
-        }
-        container.appendChild(div);
-      });
-    }
-
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-        } else {
-          entry.target.classList.remove('show');
-        }
-      });
+      container.appendChild(div);
     });
   }
+
+  observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
 }
--- WizQuizz-Angular/src/app/search/search.component.ts
+++ WizQuizz-Angular/src/app/search/search.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {ConsoleLogger} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-search',
@@ -11,71 +10,71 @@ import {ConsoleLogger} from "@angular/compiler-cli";
 export class SearchComponent implements OnInit {
   loader: HTMLElement | null = null;
 
-  ngOnInit() {
-    window.addEventListener("load", () => {
-      this.loader = document.querySelector(".loader");
-      if (this.loader) {
-        this.loader.classList.add("loader-hidden");
-        this.loader.addEventListener("transitionend", () => {
-          if (this.loader) {
-            document.body.removeChild(this.loader);
-          }
-        });
-      }
-    });
-
-    document.addEventListener('DOMContentLoaded', async () => {
-
-      const [filtersData, quizzData] = await Promise.all([
-        loadJSON({file: 'assets/jsons/play/filters_content.json'}),
-        loadJSON({file: 'assets/jsons/play/quizz_content.json'})
-      ]);
-
-      renderContent({content: filtersData.filters, containerSelector: 'aside'});
-      renderContent({content: quizzData.quizz, containerSelector: '.quizz-selection'});
-
-      const hiddenElements = document.querySelectorAll('.hidden');
-      hiddenElements.forEach((el) => observer.observe(el));
-    });
-
-    async function loadJSON({file}: { file: any }) {
-      const response = await fetch(file);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch JSON: ${response.statusText}`);
-      }
-      return response.json();
+  async ngOnInit() {
+    this.loader = document.querySelector(".loader");
+    if (this.loader) {
+      this.loader.classList.add("loader-hidden");
+      this.loader.addEventListener("transitionend", () => {
+        if (this.loader) {
+          document.body.removeChild(this.loader);
+        }
+      });
     }
 
-    function renderContent({content, containerSelector}: { content: any, containerSelector: any }) {
-      const container = document.querySelector(containerSelector);
-      content.forEach((item: any) => {
-        const div = document.createElement('div');
-        if (containerSelector === 'aside') {
-          div.classList.add('filter');
-          div.innerHTML = `<span><img src="${item.icon}" alt="NavIcon" width="64" height="64"></span>
+    try {
+      const [filtersData, quizzData] = await Promise.all([
+        this.loadJSON('assets/jsons/play/filters_content.json'),
+        this.loadJSON('assets/jsons/play/quizz_content.json')
+      ]);
+
+      this.renderContent(filtersData.filters, 'aside');
+      this.renderContent(quizzData.quizz, '.quizz-selection');
+
+      const hiddenElements = document.querySelectorAll('.hidden');
+      hiddenElements.forEach((el) => this.observer.observe(el));
+    } catch (error) {
+      console.error('Error loading JSON data:', error);
+    }
+  }
+
+  async loadJSON(file: string) {
+    console.log("Reading JSON");
+    const response = await fetch(file);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON: ${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  renderContent(content: any[], containerSelector: string) {
+    const container = document.querySelector(containerSelector);
+    if (!container) return;
+    content.forEach((item: any) => {
+      const div = document.createElement('div');
+      if (containerSelector === 'aside') {
+        div.classList.add('filter');
+        div.innerHTML = `<span><img src="${item.icon}" alt="NavIcon" width="64" height="64"></span>
                          <span>${item.text}</span>`;
-        } else if (containerSelector === '.quizz-selection') {
-          div.classList.add('quizz');
-          div.classList.add('hidden');
-          div.innerHTML = `<a href="">
+      } else if (containerSelector === '.quizz-selection') {
+        div.classList.add('quizz');
+        div.classList.add('hidden');
+        div.innerHTML = `<a href="">
                             <img src="${item.image}" width="400" height="225" class="image">
                             <h2>${item.title}</h2>
                             </a>`;
-          console.log("se ejecutó");
-        }
-        container.appendChild(div);
-      });
-    }
-
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-        } else {
-          entry.target.classList.remove('show');
-        }
-      });
+      }
+      container.appendChild(div);
     });
   }
+
+  observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
 }
