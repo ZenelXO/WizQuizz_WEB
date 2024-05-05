60ae30309e86f13396fc5008c0d2d5d7bde12d4c
Angel Hernandez <angel.hdz.ojd@gmail.com>
2024-05-05 18:26:08 +0100

    Total finished Create-Pages

--- WizQuizz-Angular/src/app/create-questions/create-questions.component.ts
+++ WizQuizz-Angular/src/app/create-questions/create-questions.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-questions',
   templateUrl: './create-questions.component.html',
   styleUrl: './create-questions.component.css'
 })
-export class CreateQuestionsComponent {
 
+export class CreateQuestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const menuIcon: HTMLElement | null = document.querySelector('.mobile-bars');
+    const mobileMenu: HTMLElement | null = document.querySelector('.mobile-menu');
+
+    if (menuIcon && mobileMenu) {
+      menuIcon.addEventListener('click', () => {
+        mobileMenu.classList.toggle('show-menu');
+      });
+    }
+
+    // Create Question
+    const submitButton: HTMLElement | null = document.getElementById("submit");
+    if (submitButton) {
+      submitButton.addEventListener("click", (event) => {
+        event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
+
+        // Obtener los valores del formulario
+        const title: string = (document.getElementById("questionTittle") as HTMLInputElement).value;
+        const answer1: string = (document.getElementById("answer1") as HTMLInputElement).value;
+        const answer2: string = (document.getElementById("answer2") as HTMLInputElement).value;
+        const answer3: string = (document.getElementById("answer3") as HTMLInputElement).value;
+        const answer4: string = (document.getElementById("answer4") as HTMLInputElement).value;
+
+        // Crear un objeto JSON con los datos
+        const data = {
+          "questionTittle": title,
+          "answer1": answer1,
+          "answer2": answer2,
+          "answer3": answer3,
+          "answer4": answer4
+        };
+
+        // Convertir el objeto JSON a una cadena JSON
+        const jsonData: string = JSON.stringify(data);
+
+        // Aquí puedes enviar jsonData a tu servidor o realizar cualquier otra operación con él
+        localStorage.setItem("questionsInfo", jsonData);
+        const localCloudQuestions: string | null = localStorage.getItem("questionsInfo");
+      });
+    }
+  }
 }
--- WizQuizz-Angular/src/app/create-quizz/create-quizz.component.ts
+++ WizQuizz-Angular/src/app/create-quizz/create-quizz.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-quizz',
   templateUrl: './create-quizz.component.html',
   styleUrl: './create-quizz.component.css'
 })
-export class CreateQuizzComponent {
 
+export class CreateQuizzComponent{
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    const menuIcon: HTMLElement | null = document.querySelector('.mobile-bars');
+    const mobileMenu: HTMLElement | null = document.querySelector('.mobile-menu');
+
+    if (menuIcon && mobileMenu) {
+        menuIcon.addEventListener('click', function () {
+            mobileMenu.classList.toggle('show-menu');
+        });
+    }
+
+    const localCloudQuestions: string | null = localStorage.getItem("questionsInfo");
+    if (localCloudQuestions === null || localCloudQuestions.length === 0) {
+        const goat = document.getElementById("goat");
+        if (goat) goat.style.display = "none";
+    } else {
+        const goat: HTMLElement | null = document.getElementById("goat");
+        const goat2: HTMLElement | null = document.getElementById("goat2");
+        if (goat && goat2) {
+            goat.style.display = "block";
+            goat2.style.display = "none";
+
+            const parsedData: { questionTittle: string, answer1: string, answer2: string, answer3: string, answer4: string } = JSON.parse(localCloudQuestions);
+            const { questionTittle, answer1, answer2, answer3, answer4 } = parsedData;
+
+            document.getElementById("questionTitleJS")!.innerHTML = questionTittle;
+            document.getElementById("answer1JS")!.innerHTML = answer1;
+            document.getElementById("answer2JS")!.innerHTML = answer2;
+            document.getElementById("answer3JS")!.innerHTML = answer3;
+            document.getElementById("answer4JS")!.innerHTML = answer4;
+        }
+    }
+
+    const finishButton: HTMLElement | null = document.getElementById("finish");
+    if (finishButton) {
+      finishButton.addEventListener("click", () => {
+        localStorage.clear();
+        this.router.navigate(['']);
+      });
+    }
+  }
+
+  onSubmit(): void {
+      // Obtener los valores del formulario
+      const titulo: string = (document.getElementById("titulo") as HTMLInputElement).value;
+      const littledescription: string = (document.getElementById("littledescription") as HTMLInputElement).value;
+
+      // Crear un objeto JSON con los datos
+      const data: { titulo: string, littledescription: string } = {
+          "titulo": titulo,
+          "littledescription": littledescription
+      };
+
+      // Convertir el objeto JSON a una cadena JSON
+      const jsonData: string = JSON.stringify(data);
+
+      // Aquí puedes enviar jsonData a tu servidor o realizar cualquier otra operación con él
+      localStorage.setItem("quizzInfo", jsonData);
+      const localCloudQuizz: string | null = localStorage.getItem("quizzInfo");
+      if (localCloudQuizz) {
+          document.getElementById("titulo")!.innerHTML = titulo;
+          document.getElementById("littledescription")!.innerHTML = littledescription;
+
+          // PRUEBAS
+          document.getElementById("prueba")!.innerHTML = localCloudQuizz;
+      }
+  }
 }
